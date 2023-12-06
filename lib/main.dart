e91b49faf35bd802f94f340e7edcfd7c3225cc66
Jun <dndud19999@gmail.com>
2023-12-06 15:31:27 +0900

    add signUp, signIn, signOut

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // svg 이미지 렌더링
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// firebase 초기화 기본 코드
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
+// 초기 화면 구성
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,6 +27,196 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
       home: MainView(),
+    );
+  }
+}
+
+class AuthenticationView extends StatefulWidget {
+  const AuthenticationView({super.key});
+
+  @override
+  State<AuthenticationView> createState() => _AuthenticationViewState();
+}
+
+class _AuthenticationViewState extends State<AuthenticationView> {
+  // FirebaseAuth 객체 가져오기
+  // TextField와 연동된 Controller 객체 가져오기 -> 텍스트 컨트롤 가능
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  // 회원 가입
+  Future<void> signUp() async {
+    try {
+      // FirebaseAuth의 create~ 메소드 -> UserCredential 객체 리턴
+      // 해당 객체의 user(not null이므로 !) 의 email, password 얻을 수 있음
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      )
+          .then((value) {
+        if (value.user!.email != null) {
+          print(value.user!.email);
+        }
+        return value;
+      });
+    } on FirebaseAuthException catch (e) {
+      if (e.message!.contains('weak-password')) {
+        print('weak-password');
+      } else if (e.message!.contains('email-already-in-use')) {
+        print('email-already-in-use');
+      } else {
+        print(e.message!);
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // 로그인
+  Future<void> signIn() async {
+    try {
+      UserCredential userCredential = await _auth
+          .signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      )
+          .then((value) {
+        if (value.user!.email != null) {
+          print(value.user!.email);
+        }
+        return value;
+      });
+    } on FirebaseAuthException catch (e) {
+      if (e.message!.contains('user-not-found')) {
+        print('user-not-found');
+      } else if (e.message!.contains('wrong-password')) {
+        print('wrong-password');
+      } else {
+        print(e.message!);
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // 로그아웃
+  Future<void> signOut() async {
+    await _auth.signOut().then((value) {
+      print('signout');
+      return value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch, // 모든 공간 채우기
+            children: [
+              SizedBox(height: 60),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: SvgPicture.asset(
+                  'assets/images/wave.svg',
+                  fit: BoxFit.contain,
+                ),
+              ), // 로고 이미지
+              SizedBox(height: 30),
+              Text('SWeetme Project',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )), // 프로젝트 설명
+              SizedBox(height: 30),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ), // _emailController 연동
+              SizedBox(height: 16),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ), // _passwordController 연동
+              SizedBox(height: 24),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    textStyle: TextStyle(fontSize: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+                onPressed: () => signIn(),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Text('Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                      )),
+                ),
+              ),  // 로그인 버튼(singIn 호출)
+              SizedBox(height: 16),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ), // password 찾기 기능(미구현)
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don\'t have an account?',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      )),
+                  TextButton(
+                    onPressed: () => signUp(),
+                    child: Text('Sign Up',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  ), // 회원 가입 버튼(signUp 호출)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
