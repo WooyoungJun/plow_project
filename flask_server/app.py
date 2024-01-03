4c9c020264f6b61f87395cd41bd8da1dd226778e
Chanjin926 <97965467+Chanjin926@users.noreply.github.com>
2024-01-03 13:40:33 +0900

    Update app.py

--- flask_server/app.py
+++ flask_server/app.py
@@ -3,8 +3,14 @@ from kocw_api import get_kocw_courses
 from summary import generate_summary
 from keywords import extract_keywords
 from textextract import text_processing
+import firebase_admin
+from firebase_admin import credentials
 
 app = Flask(__name__)
+
+# Firebase 앱 초기화
+cred = credentials.Certificate('google-services.json')
+firebase_admin.initialize_app(cred, {'storageBucket': 'gs://sweetmeproject.appspot.com'})
 
 # url 맞춰주기 : /모듈
 app.add_url_rule('/text-processing', 'text_processing', text_processing, methods=['POST'])
